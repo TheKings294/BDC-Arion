fb61a0f2468696ba5d5a5eeb3c4aa5a7df422fb3
TheKings294 <98644094+TheKings294@users.noreply.github.com>
2024-11-13 18:31:10 +0100

    Ajout de vue router

---
+++ src/Team.vue
@@ -0,0 +1,18 @@
+<script setup>
+import navBar from './components/navBar.vue';
+</script>
+
+<template>
+    <header>
+        <navBar></navBar>
+    </header>
+    <main>
+        <h1>Je suis sur la seconde page</h1>
+    </main>
+</template>
+
+<style scoped>
+h1 {
+    color: white;
+}
+</style>
