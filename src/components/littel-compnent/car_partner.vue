5f64e9cd83c055291a1178a77f54d89f4d5ebbba
TheKings294 <98644094+TheKings294@users.noreply.github.com>
2025-02-05 21:00:27 +0100

    Add struct from swipe partner

--- src/components/littel-compnent/car_partner.vue
+++ src/components/littel-compnent/car_partner.vue
@@ -1,21 +1,30 @@
 <script setup>
-
+defineProps( {
+  title: String,
+  price: String,
+  image: String,
+  description: String,
+  acroche: String
+})
 </script>
 
 <template>
   <article>
-    <h2 class="partner-title-formule"></h2>
-    <h3 class="partner-price-formule"></h3>
+    <h2 class="partner-title-formule">{{title}}</h2>
+    <h3 class="partner-price-formule">{{price}}</h3>
     <div class="partner-image-formule">
-        <img src="" alt="">
+        <img :src="image" alt="Partener image">
     </div>
     <div class="partner-description-formule">
-        <p></p>
+        <p class="acroche">{{acroche}}</p>
+        <p>{{description}}</p>
     </div>
     <a href="">Télécharger le document</a>
   </article>
 </template>
 
 <style scoped>
-
+.acroche {
+  font-style: italic;
+}
 </style>
--- src/components/partner.vue
+++ src/components/partner.vue
@@ -1,12 +1,36 @@
 <script setup>
+import car_partner from './littel-compnent/car_partner.vue'
+import formlue1 from '@/assets/img/formule+classique.webp'
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import "swiper/css/effect-coverflow";
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
+import { Navigation, Pagination, EffectCoverflow } from 'swiper/modules';
 </script>
 
 <template>
   <h1>Page Partenaria</h1>
-  <div id="carouselle-partner">
-
-  </div>
+  <swiper
+      effect="coverflow"
+      :modules="[EffectCoverflow, Pagination, Navigation]"
+      :centeredSlides="true"
+      :slidesPerView="3"
+      :coverflowEffect="{
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true
+      }"
+      :pagination="{ clickable: true }"
+      :navigation="true"
+  >
+    <swiper-slide><car_partner :title="'Test'" :price="'10 000€'" :description="'test'" :image="formlue1"></car_partner></swiper-slide>
+    <swiper-slide><car_partner :title="'Test2'" :price="'10 000€'" :description="'test2'"></car_partner></swiper-slide>
+    <swiper-slide><car_partner :title="'Test3'" :price="'10 000€'" :description="'test3'"></car_partner></swiper-slide>
+  </swiper>
 </template>
 
 <style scoped>
