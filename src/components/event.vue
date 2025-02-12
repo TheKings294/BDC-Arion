f7a45c8f1c7dad265eec764cefb4f7fb8457e3ae
TheKings294 <98644094+TheKings294@users.noreply.github.com>
2025-02-12 12:25:49 +0100

    Ajout Event page

--- src/components/event.vue
+++ src/components/event.vue
@@ -8,24 +8,20 @@ onMounted(() => {
 </script>
 
 <template>
-<<<<<<< Updated upstream
-
-  <h1>Page Event</h1>
-  <div id="time-line-event">
-
+  <div class="container-fluid">
+    <div class="row">
+      <h1 class="text-center mt-3">Nos Évènements</h1>
+      <div id="time-line-event" class="text-center">
+        <img :src="time" alt="eventTime" class="w-80">
+      </div>
+    </div>
+    <div id="description-event" class="row d-flex flex-wrap align-items-center mt-5 flex-column">
+      <event_description :title="'Event 1'" :content="'description de l\'event'" :back="'#725643'" :color="'white'"></event_description>
+      <event_description :title="'Event 2'" :content="'description de l\'event'" :back="'#F8DF8F'" :color="'black'"></event_description>
+      <event_description :title="'Event 3'" :content="'description de l\'event'" :back="'#725643'" :color="'white'"></event_description>
+      <event_description :title="'Event 4'" :content="'description de l\'event'" :back="'#F8DF8F'" :color="'black'"></event_description>
+    </div>
   </div>
-  <div id="description-event">
-
-  </div>
-=======
-  <h1 class="text-center mt-3">Nos Évènements</h1>
-  <div id="time-line-event" class="text-center">
-    <img :src="time" alt="eventTime" class="w-80">
-  </div>
-  <div id="description-event" class="d-flex flex-wrap">
-    
-  </div>    
->>>>>>> Stashed changes
 </template>
 
 <style scoped>
--- src/components/littel-compnent/event_description.vue
+++ src/components/littel-compnent/event_description.vue
@@ -2,18 +2,24 @@
 defineProps({
   title: String,
   content: String,
-  back: String
+  back: String,
+  color: String
     }
 )
 </script>
 
 <template>
-  <article :style="{backgroundColor: back}">
-    <h4 class="fs-4">{{title}}</h4>
-    <p>{{content}}</p>
+  <article :style="{backgroundColor: back, color: color}" class="component rounded mb-3">
+    <h4 class="fs-4 text-center">{{title}}</h4>
+    <p class="text-center">{{content}}</p>
   </article>
 </template>
 
 <style scoped>
-
+@media (min-width: 1250px) {
+  .component {
+    width: 700px;
+    height: 200px;
+  }
+}
 </style>
