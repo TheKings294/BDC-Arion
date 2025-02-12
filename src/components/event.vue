1e5b1baa409abe263d1827800f656fe239040067
TheKings294 <98644094+TheKings294@users.noreply.github.com>
2025-02-12 11:57:10 +0100

    Auto stash before merge of "Developement" and "origin/Developement"

--- src/components/event.vue
+++ src/components/event.vue
@@ -1,8 +1,14 @@
 <script setup>
-
+import {onMounted} from "vue";
+import time from '@/assets/img/version+ordi+event.webp'
+import event_description from './littel-compnent/event_description.vue'
+onMounted(() => {
+  document.querySelector('body').style.background = '#fff'
+})
 </script>
 
 <template>
+<<<<<<< Updated upstream
 
   <h1>Page Event</h1>
   <div id="time-line-event">
@@ -11,6 +17,15 @@
   <div id="description-event">
 
   </div>
+=======
+  <h1 class="text-center mt-3">Nos Évènements</h1>
+  <div id="time-line-event" class="text-center">
+    <img :src="time" alt="eventTime" class="w-80">
+  </div>
+  <div id="description-event" class="d-flex flex-wrap">
+    
+  </div>    
+>>>>>>> Stashed changes
 </template>
 
 <style scoped>
--- src/components/littel-compnent/event_description.vue
+++ src/components/littel-compnent/event_description.vue
@@ -1,9 +1,16 @@
 <script setup>
-
+defineProps({
+  title: String,
+  content: String,
+  back: String
+    }
+)
 </script>
 
 <template>
-  <article>
+  <article :style="{backgroundColor: back}">
+    <h4 class="fs-4">{{title}}</h4>
+    <p>{{content}}</p>
   </article>
 </template>
 
